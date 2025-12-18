704793029703d3ee96c0b39487304e498af423a5
dpkn1989 <dpkn1989@gmail.com>
2025-12-18 17:39:19 +0100

    Installation of libraries & finding them from an application

---
+++ process_findingLibrary/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "car_lib/Car.h"
+
+int main(int agrc, char* argv[])
+{
+   std::cout << "Om Ganesaya namaha!" << std::endl;
+
+   Car C1;
+   Car C2("TATA", "Black", 150.0f);
+
+   C1.accelerate();
+   C1.accelerate();
+
+   C2.accelerate();
+   C2.brake();
+
+   C1.displayStatus();
+   C2.displayStatus();
+   return 0;
+}
